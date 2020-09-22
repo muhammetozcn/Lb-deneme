78b2f134d21b406055e4506e18aced8a000935c3
Muhammet Ozcan <160201027@kocaeli.edu.tr>
2020-09-22 12:21:24 +0300

    f

---
+++ lb-prod/src/datasources/index.ts
@@ -0,0 +1,1 @@
+export * from './pldb.datasource';
---
+++ lb-prod/src/models/employee.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Employee extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  Id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  City: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  salary: number;
+
+
+  constructor(data?: Partial<Employee>) {
+    super(data);
+  }
+}
+
+export interface EmployeeRelations {
+  // describe navigational properties here
+}
+
+export type EmployeeWithRelations = Employee & EmployeeRelations;
